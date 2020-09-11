0b1e7e0b51a6f571934c896d607d3a6600b8bd85
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2020-09-10 20:54:36 -0400

    Create PizzaBot.cpp

---
+++ PizzaBot.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+int main()
+{
+    
+    int slices;
+    cout << "How mamy slices of pizza do you want to eat?" << std::endl;
+    cin >> slices;
+    cout << "You have "<<slices<<"  slices of pizza." << std::endl;
+}
