a868d0b2d957fe5484f0c8a8873eea7c0ef1fe83
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2020-09-10 20:46:10 -0400

    Create DeclaritionInitialization.cpp

---
+++ DeclaritionInitialization.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+int main()
+{
+    int slices; // Declariation
+    slices = 5; //initialization
+
+    
+    int children = slices; //Declariation - Initialization
+
+    cout << children;
+}
